e84d62312637fd8a1094e83ff307066ad51d9a74
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-08-27 13:23:11 -0300

    [ADD] Testando novo módulo cashflow

--- mis_builder_cash_flow_analytic/report/mis_cash_flow.py
+++ mis_builder_cash_flow_analytic/report/mis_cash_flow.py
@@ -8,13 +8,13 @@ class MisCash_flow(models.Model):
 
     _inherit = 'mis.cash_flow'
 
-    analytic_account_id = fields.Many2one(
-        comodel_name='account.analytic.account',
-        string='Analytic Account',
-        auto_join=True,
-        index=True,
-        readonly=True,
-    )
+    # analytic_account_id = fields.Many2one(
+    #     comodel_name='account.analytic.account',
+    #     string='Analytic Account',
+    #     auto_join=True,
+    #     index=True,
+    #     readonly=True,
+    # )
 
     @api.model_cr
     def init(self):
@@ -38,7 +38,7 @@ class MisCash_flow(models.Model):
                     THEN -aml.balance
                     ELSE 0.0
                 END AS credit,
-                aml.analytic_account_id as analytic_account_id,
+                -- aml.analytic_account_id as analytic_account_id,
                 aml.reconciled as reconciled,
                 aml.full_reconcile_id as full_reconcile_id,
                 aml.partner_id as partner_id,
@@ -48,30 +48,30 @@ class MisCash_flow(models.Model):
                 aml.date_maturity as date
             FROM account_move_line as aml
             UNION ALL
-            SELECT
+             SELECT
                 fl.id as id,
                 CAST('forecast_line' AS varchar) as line_type,
-                Null as move_line_id,
-                fl.account_id as account_id,
-                CASE
-                    WHEN fl.balance > 0
-                    THEN fl.balance
-                    ELSE 0.0
-                END AS debit,
-                CASE
-                    WHEN fl.balance < 0
-                    THEN -fl.balance
-                    ELSE 0.0
-                END AS credit,
-                fl.analytic_account_id as analytic_account_id,
-                Null as reconciled,
-                Null as full_reconcile_id,
+                NULL as move_line_id,
+                fi.account_id as account_id,
+                    CASE
+                        WHEN fi.type = 'out_invoice' THEN fl.price_total
+                        ELSE 0.0
+                    END AS debit,
+                    CASE
+                        WHEN fi.type = 'in_invoice' THEN - fl.price_total
+                        ELSE 0.0
+                    END AS credit,
+                -- fl.account_analytic_id as analytic_account_id,
+                NULL AS reconciled,
+                NULL AS full_reconcile_id,
                 fl.partner_id as partner_id,
                 fl.company_id as company_id,
-                %i as user_type_id,
+                %i AS user_type_id,
                 fl.name as name,
-                fl.date as date
-            FROM mis_cash_flow_forecast_line as fl
+                fi.date_invoice as date
+               FROM account_invoice_line fl
+               INNER JOIN account_invoice fi on fl.invoice_id = fi.id
+               WHERE fi.state = 'draft'
         """ % account_type_receivable.id
         tools.drop_view_if_exists(self.env.cr, self._table)
         self._cr.execute(
